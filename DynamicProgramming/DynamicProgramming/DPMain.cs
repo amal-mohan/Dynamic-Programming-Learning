bb4d9c7199973f60bee07e57942c70695c47971b
amal-mohan <amal.phelsea@gmail.com>
2018-03-29 21:00:15 -0500

    added top down approach for calculating Fibonacci series

---
+++ DynamicProgramming/DynamicProgramming/DPMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class DynamicPro
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Dynamic Programming!!");
+            Console.WriteLine("1. Generate Fibonacci Series");
+            string option=Console.ReadLine();
+            switch(option)
+            {
+                case "1":
+                    Console.WriteLine("Enter N for which Fibonacci series is to be calculated");
+                    int N = Convert.ToInt32(Console.ReadLine());
+                    Fibonacci fib = new Fibonacci(N);
+                    int fibonac = fib.GenerateFib();
+                    Console.WriteLine("Fib("+N+")="+fibonac);
+                    Console.ReadKey();
+                    break;
+                default: Console.WriteLine("Invalid choice");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DynamicProgramming/DynamicProgramming/Problems/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    class Fibonacci
+    {
+        private int n;
+
+        public Fibonacci(int n)
+        {
+            this.n = n;
+        }
+
+        internal int GenerateFib()
+        {
+            int[] FibStore = new int[n];
+            FibStore[0] = 1;
+            FibStore[1] = 1;
+            for(int i=2;i<n;i++)
+            {
+                FibStore[i] = FibStore[i - 1] + FibStore[i - 2];
+            }
+            return FibStore[n-1];
+        }
+    }
+}
